f0eeb754ae27121e8e0bd30957f2e04085a8d3ea
”名前” <>
2019-11-05 14:16:37 +0000

    login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def index
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2019_11_05_075158) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
