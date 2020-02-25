d825e23653e23733bf02a384b33490733775bc28
”名前” <>
2020-02-26 06:40:15 +0900

    quiz function preparing

--- app/controllers/qs_controller.rb
+++ app/controllers/qs_controller.rb
@@ -1,5 +1,14 @@
 class QsController < ApplicationController
   def index
     @qs = Q.where(category_id: params[:category_id])
+    @category = Category.find(params[:category_id])
   end
+  
+  def new
+    
+    @q = Q.new
+    @category = Category.find(params[:category_id])
+    
+  end
+  
 end
