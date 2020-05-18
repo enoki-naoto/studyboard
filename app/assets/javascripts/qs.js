9b90cecf6e276819def803f84bf84131da28f379
”名前” <>
2020-05-18 12:12:26 +0900

    test

--- app/assets/javascripts/qs.js
+++ app/assets/javascripts/qs.js
@@ -2,18 +2,18 @@
 
 {
 
-  const question = document.getElementById('question');
+  const quest = document.getElementById('quest');
   const choices1 = document.getElementById('choices1');
   const choices2 = document.getElementById('choices2');
   const choices3 = document.getElementById('choices3');
   const btn = document.getElementById('btn');
   
-  // const quiz = gon.qcontents;
+  const quiz = gon.qcontents;
   let currentNum = 0;
-  question.textContent = gon.qcontents[currentNum].qsentence;
-  choices1.textContent = gon.qcontents[currentNum].choice1;
-  choices2.textContent = gon.qcontents[currentNum].choice2;
-  choices3.textContent = gon.qcontents[currentNum].choice3;
+  quest.textContent = quiz[currentNum].qsentence;
+  choices1.textContent = quiz[currentNum].choice1;
+  choices2.textContent = quiz[currentNum].choice2;
+  choices3.textContent = quiz[currentNum].choice3;
   
   
   
